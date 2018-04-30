6fc7b9690f5f3eb73f55260bbb296d896b974fd5
Aiman Danish <aimanished@gmail.com>
2018-04-30 19:43:14 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a16031940/basicpiano/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.a16031940.basicpiano;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static int SPLAsH_TIME_OUT = 4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this,PianoActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        },SPLAsH_TIME_OUT);
+
+    }
+}
